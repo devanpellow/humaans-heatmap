66f9b3a31837f325568b3d4233e598882b51656b
Devan Pellow <devanpellow@gmail.com>
2021-04-21 23:17:50 -0400

    Put employees fetched lat and lng in employee object

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,46 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
 
 
 export default function Map({employeesList, center, zoom}) {
 
+  const googleMapsKey = process.env.REACT_APP_API_KEY_GOOGLE_MAPS
 
-  console.log(employeesList)
+  const [employeeListWithLatLng, setEmployeeListWithLatLng] = useState([])
+  const [loading, setLoading] = useState(false);
+
+  const fetchEmployeeLatLng = async (employee) => {
+    setLoading(true)
+    const res = await fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + employee.remoteCity + employee.remoteCountry + '&key=' + googleMapsKey)
+    const {results} = await res.json()   
+    setLoading(false)
+    return {...employee, results}
+  }
+  
+  const getEmployeesLocation = () => {
+    const employees = new Map([
+      employeesList.map(employee => fetchEmployeeLatLng(employee))
+    ])
+    setEmployeeListWithLatLng(employees)
+  }
+
+  useEffect(() => {
+    getEmployeesLocation()
+  })
 
 
   return (
     <div className="map">
       <GoogleMapReact
-        bootstrapURLKeys={{key: process.env.REACT_APP_API_KEY_GOOGLE_MAPS}}
+        bootstrapURLKeys={{key: googleMapsKey}}
         defaultCenter={center}
         defaultZoom={zoom}
       >
+        {/* {employeeListWithLatLng && 
+          employeeListWithLatLng.map(marker => <LocationMarker lat={marker} lng={marker}/>)
+        }  */}
+        
         <LocationMarker lat={55.688035} lng={12.5609429}/>
       </GoogleMapReact>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,20 @@ import Map from '../components/Map';
 import Loading from '../components/Loading';
 
 export default function Home() {
+  const humaansURL = process.env.REACT_APP_API_HUMAANS_URL
+  const humaansKey = process.env.REACT_APP_API_KEY_HUMAANS
+  
   const [employeesList, setEmployeesList] = useState([]);
   const [remoteEmployeesList, setRemoteEmployeesList] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const fetchAllEmployees = async () => {
     setLoading(true);
-    const res = await fetch(process.env.REACT_APP_API_HUMAANS_URL, {
+    const res = await fetch(humaansURL, {
       method: 'GET',
       headers: {
         Accept: 'application/json',
-        Authorization: 'Bearer ' + process.env.REACT_APP_API_KEY_HUMAANS,
+        Authorization: 'Bearer ' + humaansKey,
       },
     });
     const { data } = await res.json();
